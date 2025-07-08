9cc06cd3ac407412ec979ee9e669aaa42622f068
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-07-08 06:55:31 -0300

    Excluida pasta Response

--- APPS/api/back-end/entra21-atividade-crud-api/API_HealthGo/Response/AvaliacaoGetAllResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using API_HealthGo.Entity;
-
-namespace API_HealthGo.Response
-{
-    public class AvaliacaoGetAllResponse
-    {
-        public IEnumerable<AvaliacaoEntity> Data { get; set; }
-    }
-}
