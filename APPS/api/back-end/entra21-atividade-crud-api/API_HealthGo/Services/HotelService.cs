ea9cefabb95defcb7702097407d1c11534678f1b
Nícolas Santos <nicolasps.contato@gmail.com>
2025-07-25 14:10:48 -0300

    Revert "back-end adjustment for adding hotel"
    This reverts commit a53c57b00b656223c416e37b356ca9a4c2ebf7f5.

--- APPS/api/back-end/entra21-atividade-crud-api/API_HealthGo/Services/HotelService.cs
+++ APPS/api/back-end/entra21-atividade-crud-api/API_HealthGo/Services/HotelService.cs
@@ -3,20 +3,16 @@ using API_HealthGo.Contracts.Service;
 using API_HealthGo.DTO;
 using API_HealthGo.Entities;
 using API_HealthGo.Responses;
-using Microsoft.AspNetCore.Http;
-using System.Security.Claims;
 
 namespace API_HealthGo.Services
 {
     public class HotelService : IHotelService
     {
-        private readonly IHotelRepository _repository;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private IHotelRepository _repository;
 
-        public HotelService(IHotelRepository repository, IHttpContextAccessor httpContextAccessor)
+        public HotelService(IHotelRepository repository)
         {
             _repository = repository;
-            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<HotelGetAllResponse> GetAll()
@@ -32,34 +28,8 @@ namespace API_HealthGo.Services
             return await _repository.GetHotelById(id);
         }
 
-        public async Task<MessageResponse> Post(HotelInsertDTO hotelDto)
+        public async Task<MessageResponse> Post(HotelInsertDTO hotel)
         {
-            var userIdClaim = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier);
-            if (userIdClaim == null || !int.TryParse(userIdClaim.Value, out int userId))
-            {
-                throw new Exception("ID do usuário não encontrado no token.");
-            }
-
-            var hotel = new HotelInsertDTO
-            {
-                CNPJ = hotelDto.CNPJ,
-                Nome = hotelDto.Nome,
-                Tipo = hotelDto.Tipo,
-                Email = hotelDto.Email,
-                Telefone = hotelDto.Telefone,
-                Site = hotelDto.Site,
-                Acessibilidade = hotelDto.Acessibilidade,
-                CEP = hotelDto.CEP,
-                Bairro = hotelDto.Bairro,
-                Rua = hotelDto.Rua,
-                NumeroEndereco = hotelDto.NumeroEndereco,
-                Descricao = hotelDto.Descricao,
-                Ativo = hotelDto.Ativo,
-                DataInicio = hotelDto.DataInicio,
-                Cidade_Id = hotelDto.Cidade_Id,
-                Pessoa_Id = userId
-            };
-
             await _repository.Insert(hotel);
             return new MessageResponse
             {
