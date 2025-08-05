407fcaba24fb1f0af2a053d123247e9de1577980
Lucas José <lucasjds012@gmail.com>
2025-08-05 18:44:33 -0300

    api: consertado o using que agora é necessário com o MessageResponse

--- APPS/api/back-end/entra21-atividade-crud-api/API_HealthGo/Controllers/AuthController.cs
+++ APPS/api/back-end/entra21-atividade-crud-api/API_HealthGo/Controllers/AuthController.cs
@@ -1,7 +1,7 @@
 ﻿using API_HealthGo.Contracts.Repositories;
 using API_HealthGo.Contracts.Service;
 using API_HealthGo.DTO;
-using API_HealthGo.Responses;
+using API_HealthGo.Responses.MessageResponse;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_HealthGo.Controllers
