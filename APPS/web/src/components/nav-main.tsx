48fb6dcc61a5a665f226d73cf421affd05023510
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-09-10 08:52:31 -0300

    sidebar adjustment

--- APPS/web/src/components/nav-main.tsx
+++ APPS/web/src/components/nav-main.tsx
@@ -17,28 +17,14 @@ export function NavMain({ items, pathname }: { items: { title: string; url: stri
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
-            >
-              <PlusCircle />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
-            <Button
-              size="icon"
-              className="size-8 group-data-[collapsible=icon]:opacity-0"
-              variant="outline"
-            >
-              <Mail />
-              <span className="sr-only">Inbox</span>
-            </Button>
+            
+
           </SidebarMenuItem>
         </SidebarMenu>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
                 <Link to={item.url}>
-                  {/* A LINHA ABAIXO FOI ALTERADA */}
                   <SidebarMenuButton tooltip={item.title} isActive={pathname === item.url}>
                     {item.icon && <item.icon />}
                     <span>{item.title}</span>
